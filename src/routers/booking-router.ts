b96ea3e3547089b7f10f843a1d8bd04f44e64c9a
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-07-21 22:22:58 -0300

    Fix: Get Booking

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
+import { getBooking } from '@/controllers/booking-controller';
 
 const bookingRouter = Router();
 
 bookingRouter
   .all('/*', authenticateToken)
   .get('/health', (_req, res) => res.send('Booking OK!'))
-  .get('/')
+  .get('/', getBooking)
+  .post('/')
 
 
 export { bookingRouter };
